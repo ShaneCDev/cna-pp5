26d550c0a1c7fbf65c9fefeb8ca4dc6889e93ec8
Shane <shaneconway1998@gmail.com>
2023-09-30 20:59:31 +0100

    updated DNS on blacknight, should take effect in about an hour

--- cna_ireland/settings.py
+++ cna_ireland/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'cnaireland.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'cnaireland.com', 'cna-ireland-c498f99b12b3.herokuapp.com']
 
 
 # Application definition
